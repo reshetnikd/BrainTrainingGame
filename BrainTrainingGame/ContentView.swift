8cf67f7de186076908a48c3790b5b3f460dd95a7
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-22 23:16:46 +0200

    Add response logic and UI

--- BrainTrainingGame/ContentView.swift
+++ BrainTrainingGame/ContentView.swift
@@ -9,8 +9,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var appCurrentChoice = Int.random(in: 0..<2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    
+    let possibleMoves = ["✊🏻", "✋🏻", "✌🏻"]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Score: \(score)")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Text("\(possibleMoves[appCurrentChoice])")
+                .font(.system(size: 64))
+            
+            Text("You should \(shouldWin ? "Win" : "Lose")")
+            
+            HStack {
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.playerResponse(number)
+                    }) {
+                        Text(self.possibleMoves[number])
+                            .font(.system(size: 64))
+                    }
+                }
+            }
+        }
+    }
+    
+    func playerResponse(_ number: Int) {
+        if shouldWin {
+            switch appCurrentChoice {
+            case 0:
+                if number == 1 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            case 1:
+                if number == 2 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            case 2:
+                if number == 0 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            default:
+                break
+            }
+        } else {
+            switch appCurrentChoice {
+            case 0:
+                if number == 2 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            case 1:
+                if number == 0 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            case 2:
+                if number == 1 {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+            default:
+                break
+            }
+        }
     }
 }
 
